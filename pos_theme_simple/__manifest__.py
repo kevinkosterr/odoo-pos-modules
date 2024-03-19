be0c061f4977a22f5fde743454c046306683f1ad
kevinkosterr <koster.k2001@gmail.com>
2024-03-19 21:35:00 +0100

    (module) updated manifest file

--- pos_theme_simple/__manifest__.py
+++ pos_theme_simple/__manifest__.py
@@ -4,7 +4,18 @@
     "name": "Simple POS Theme",
     "summary": "A simple POS theme",
     "description": """
-        A simple POS theme that includes some tweaks to the POS UI.
+        ## Odoo 17.0 - Simple POS Theme
+        This is a Odoo 17.0 module that aims to make some small improvements on the
+        UI of the Odoo 17.0 `point_of_sale` module. As if right now, the module consist
+        of nothing fancy. However, it does make some noticeable changes to the general lay-out
+        of the POS system. 
+
+        ### Tweaks
+        Some of the changes include:
+        - Lay-out of the product screen has been mirrored;
+        - Lay-out of the payment screen has been mirrored;
+        - Image visibility is improved for categories and products;
+        - Simplistic 'class-based' dark mode.
     """,
     "author": "Kevin Koster",
     "website": "https://kevinkoster.nl",
@@ -12,8 +23,6 @@
     "version": "0.1",
     "depends": ["point_of_sale"],
     "assets": {
-        # "web.assets_backend": [
-        # ],
         "point_of_sale._assets_pos": [
             "/pos_theme_simple/static/src/**/*",
         ],
