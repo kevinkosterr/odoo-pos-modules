0f5c9c76daff10ee5d049c0c50f8300598bb085b
kevinkosterr <koster.k2001@gmail.com>
2024-03-22 11:18:51 +0100

    [pos_theme_simple] update manifest description

--- pos_theme_simple/__manifest__.py
+++ pos_theme_simple/__manifest__.py
@@ -4,16 +4,13 @@
     "name": "Simple POS Theme",
     "summary": "A simple POS theme",
     "description": """
-        ## Odoo 17.0 - Simple POS Theme
-        This is a Odoo 17.0 module that aims to make some small improvements on the
+        This is an Odoo 17.0 module that aims to make some small improvements on the
         UI of the Odoo 17.0 `point_of_sale` module. As if right now, the module consist
         of nothing fancy. However, it does make some noticeable changes to the general lay-out
         of the POS system. 
 
-        ### Tweaks
         Some of the changes include:
-        - Lay-out of the product screen has been mirrored;
-        - Lay-out of the payment screen has been mirrored;
+        - Lay-out of several screens have been mirrored;
         - Image visibility is improved for categories and products;
         - Simplistic 'class-based' dark mode.
     """,
